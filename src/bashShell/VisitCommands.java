66721192fe83da91d1f667125e5ab7a63699a532
jakebyrd <byrdjake126@gmail.com>
2019-12-09 08:38:11 -0600

    visit terminal

--- src/bashShell/VisitCommands.java
+++ src/bashShell/VisitCommands.java
@@ -48,48 +48,63 @@ public class VisitCommands implements Visitor {
     }
 
     public Object visitTerminal(Terminal terminal, Object object) {
-
+        String spelling = terminal.getSpelling();
+        //return executable
+        List<String> executables = new ArrayList<>(Arrays.asList("cat", "ls", "pwd", "touch", "cp", "mv", "rm", "chmod", "man", "ps", "bg", "mkdir", "test", "cd"));
+        if (executables.contains(spelling)){
+            terminal.type = Type.executable;
+        }
+        else{
+            terminal.type = Type.numeric;
+            //return numeric
+            List<Character> ints = new ArrayList<>(Arrays.asList('0','1','2','3','4','5','6','7','8','9'));
+            for (char ch: spelling.toCharArray()){
+                if (!ints.contains(ch)) {
+                    terminal.type = Type.string;
+                    break;
+                }
+            }
     }
-
+    //check if script is legal
     public Object visitScript(Script script, Object object) {
         script.getC().accept(this, object);
         return null;
     }
-
+    //check each command
     public Object visitSeqCmd(SeqCmd seqCmd, Object object) {
         seqCmd.getC1().accept(this, object);
         seqCmd.getC2().accept(this, object);
         return null;
     }
-
+    //check each arguement
     public Object visitSeqArg(SeqArg arg, Object object){
         arg.getArg1().accept(this, object);
         arg.getArg2().accept(this, object);
         return null;
     }
-
+    //return fname type
     public Object visitFNameArg(FNameArg fNameArg, Object object) {
         Type vType = (Type) fNameArg.getTerm().accept(this, object);
         fNameArg.type = vType;
         return fNameArg.type;
     }
-
+    //return lit type
     public Object visitLiteralArg(LiteralArg literalArg, Object object) {
         Type argType = (Type) literalArg.getTerm().accept(this, object);
         literalArg.type = argType;
         return argType;
     }
-
+    //return var type
     public Object visitVarArg(VarArg varArg, Object object) {
         Type vType = (Type) varArg.getTerm().accept(this, object);
         varArg.type = vType;
         return varArg.type;
     }
-
+    //return null type
     public Object visitNullArg(NullArg nullArg, Object object) {
         return Type.nulltype;
     }
-
+    //return null
     public Object visitNullCmd(NullCmd nullCmd, Object object) {
         return null;
     }
